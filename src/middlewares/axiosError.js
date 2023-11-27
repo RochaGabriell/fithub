6eda66ac9fc7c6a27f2a73921e6b0c0167e189b9
RochaGabriell <rochagabriell.dev@gmail.com>
2023-11-27 09:51:42 -0300

    Fix a bug

--- src/middlewares/axiosError.js
+++ src/middlewares/axiosError.js
@@ -1,15 +1,15 @@
-import { useNavigate } from 'react-router-dom'
-
 import api from '../services/api'
 
 /**
   Este middleware intercepta os erros de autenticação e tenta renovar o token de acesso com o token de refresh. Caso o token de refresh esteja expirado, o usuário é deslogado.
 */
+let refresh = false
+
 api.interceptors.response.use(
   resp => resp,
   async error => {
-    console.log(error)
-    if (error.response.status === 401) {
+    if (error.response.status === 401 && !refresh) {
+      refresh = true
       const data_local = JSON.parse(localStorage.getItem('authTokens'))
       const response = await api.post('/account/token/refresh/', {
         refresh: data_local.refresh
@@ -19,12 +19,11 @@ api.interceptors.response.use(
         api.defaults.headers.Authorization = `Bearer ${response.data.access}`
         localStorage.setItem('authTokens', JSON.stringify(response.data))
         return api(error.config)
-      } else {
-        localStorage.removeItem('authTokens')
-        const navigate = useNavigate()
-        navigate('/login')
-        return error
       }
     }
+    refresh = false
+    api.defaults.headers.Authorization = null
+    localStorage.removeItem('authTokens')
+    return error
   }
 )
--- src/router/privateRoute.jsx
+++ src/router/privateRoute.jsx
@@ -3,15 +3,14 @@ import { Navigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
-const PrivateRoute = ({ children, ...rest }) => {
+const PrivateRoute = props => {
   const { user } = useContext(AuthContext)
 
-  return !user ? <Navigate to="/login" /> : children
+  return !user ? <Navigate to="/login" /> : props.children
 }
 
 PrivateRoute.propTypes = {
-  children: PropTypes.node.isRequired,
-  rest: PropTypes.any
+  children: PropTypes.node.isRequired
 }
 
 export default PrivateRoute
