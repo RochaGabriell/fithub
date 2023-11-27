095251a130e55e83dad54911663956ae4de4e1a6
RochaGabriell <rochagabriell.dev@gmail.com>
2023-11-27 09:02:33 -0300

    Fix a bug

--- src/middlewares/axiosError.js
+++ src/middlewares/axiosError.js
@@ -11,20 +11,16 @@ api.interceptors.response.use(
     console.log(error)
     if (error.response.status === 401) {
       const data_local = JSON.parse(localStorage.getItem('authTokens'))
-      if (data_local) {
-        const response = await api.post('/account/token/refresh/', {
-          refresh: data_local.refresh
-        })
+      const response = await api.post('/account/token/refresh/', {
+        refresh: data_local.refresh
+      })
 
-        if (response.status === 200) {
-          api.defaults.headers.Authorization = `Bearer ${response.data.access}`
-          localStorage.setItem('authTokens', JSON.stringify(response.data))
-          return api(error.config)
-        } else {
-          localStorage.removeItem('authTokens')
-          return error
-        }
+      if (response.status === 200) {
+        api.defaults.headers.Authorization = `Bearer ${response.data.access}`
+        localStorage.setItem('authTokens', JSON.stringify(response.data))
+        return api(error.config)
       } else {
+        localStorage.removeItem('authTokens')
         const navigate = useNavigate()
         navigate('/login')
         return error
